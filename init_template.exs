7213aca2afa2b72373cbda730ed755129675c9bf
Alexei Sholik <alcosholik@gmail.com>
2014-08-29 04:12:29 +0300

    Add conditional templates

--- init_template.exs
+++ init_template.exs
@@ -1,1 +1,5 @@
 param :title_underline, String.duplicate("=", String.length(config[:MODULE_NAME]))
+flag :sup, :boolean
+
+select "app_with_sup.ex", if: flags[:sup], rename: "{{APP_NAME}}.ex"
+select "empty_app.ex", if: not flags[:sup], rename: "{{APP_NAME}}.ex"
