e0bd128fac074d05b1e49947679317db8059899e
Alexei Sholik <alcosholik@gmail.com>
2014-09-11 04:14:30 +0300

    Update template paths

--- _template_config/init.exs
+++ _template_config/init.exs
@@ -1,7 +1,7 @@
 param :title_underline, String.duplicate("=", String.length(config[:MODULE_NAME]))
 
 if flags[:sup] do
-  select "app_with_sup.ex", rename: "{{APP_NAME}}.ex"
+  select "lib/app_with_sup.ex.template", rename: "lib/{{APP_NAME}}.ex"
 else
-  select "empty_app.ex", rename: "{{APP_NAME}}.ex"
+  select "lib/empty_app.ex.template", rename: "lib/{{APP_NAME}}.ex"
 end
